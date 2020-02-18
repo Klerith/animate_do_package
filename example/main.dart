2184bdf741d013653056acd5796a31a4edd3efa9
Fernando <fernando.herrera85@gmail.com>
2020-02-18 12:42:18 -0500

    Added example

---
+++ example/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        body: Center(
+          child: BounceInDown(
+            child: Rectangulo()
+          )
+        ),
+      ),
+    );
+  }
+}
+
+class Rectangulo extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.blueAccent,
+      ),
+    );
+  }
+}
