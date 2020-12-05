595a53a6e11cd2cbc2c2a6aef7f9c2a96f2020cc
Fernando Herrera <fernando.herrera85@gmail.com>
2020-12-05 15:58:03 -0500

    dartfmt -w . formatted

--- example/main.dart
+++ example/main.dart
@@ -2,25 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:animate_do/animate_do.dart';
 
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
-        body: Center(
-          child: BounceInDown(
-            child: Square()
-          )
-        ),
+        body: Center(child: BounceInDown(child: Square())),
       ),
     );
   }
 }
 
 class Square extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Container(
--- test/animate_do_test.dart
+++ test/animate_do_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:animate_do/animate_do.dart';
 
 void main() {
-
   // Testing in the future
 
   // test('adds one to input values', () {
