19e859fe257ea18e2d271645dfbf1d3d54e54f81
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-31 20:30:33 -0500

    Refactor AnimateDoTypes test in animate_do_types_test.dart

--- test/types/animate_do_types_test.dart
+++ test/types/animate_do_types_test.dart
@@ -1,17 +1,13 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-
-
 void main() {
-
-  test('should ensure the types AnimateDo Types', () {
-         
-    expect(AnimateDoDirection.forward.toString(), 'AnimateDoDirection.forward');
-    expect(AnimateDoDirection.backward.toString(), 'AnimateDoDirection.backward');
-
-
+  group('AnimateDoTypes', () {
+    test('should ensure the types AnimateDo Types', () {
+      expect(
+          AnimateDoDirection.forward.toString(), 'AnimateDoDirection.forward');
+      expect(AnimateDoDirection.backward.toString(),
+          'AnimateDoDirection.backward');
+    });
   });
-
-
 }
