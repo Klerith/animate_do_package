0cbfcbe37c7b7e18be4aa256605952689561b4a0
Alejandro Quiles Lopez <quiles.lopez.alejandro@gmail.com>
2024-12-12 00:27:47 -0500

    add base ShakeXY animation

--- lib/src/animations/animate_do_attention_seekers.dart
+++ lib/src/animations/animate_do_attention_seekers.dart
@@ -851,18 +851,17 @@ class RouletteState extends State<Roulette>
   }
 }
 
+/// Class [ShakeXY]:
 /// [key]: optional widget key reference
 /// [child]: mandatory, widget to animate
 /// [duration]: how much time the animation should take
 /// [delay]: delay before the animation starts
 /// [controller]: optional/mandatory, exposes the animation controller created by Animate_do
-/// [manualTrigger]: boolean that indicates if you want to trigger the animation manually with the controller
-/// [animate]: For a State controller property, if you re-render changing it from false to true, the animation will be fired immediately
-/// [onFinish]: callback that returns the direction of the animation, [AnimateDoDirection.forward] or [AnimateDoDirection.backward]
-/// [curve]: curve for the animation
-/// [infinite]: loops the animation until the widget is destroyed
-/// [spins]: how many spins you want to do
-class ShakeX extends StatefulWidget {
+/// the controller can be use to repeat, reverse and anything you want, its just an animation controller
+/// [fromX] from where you want to start the animation X
+/// [fromY] from where you want to start the animation Y
+/// [infinite] loops the animation until the widget is destroyed
+class ShakeXY extends StatefulWidget {
   final Widget child;
   final Duration duration;
   final Duration delay;
@@ -872,9 +871,10 @@ class ShakeX extends StatefulWidget {
   final bool animate;
   final Function(AnimateDoDirection direction)? onFinish;
   final Curve curve;
-  final double from;
+  final double fromX;
+  final double fromY;
 
-  ShakeX(
+  ShakeXY(
       {key,
       required this.child,
       this.duration = const Duration(milliseconds: 1000),
@@ -883,7 +883,8 @@ class ShakeX extends StatefulWidget {
       this.controller,
       this.manualTrigger = false,
       this.animate = true,
-      this.from = 20,
+      this.fromX = 20,
+      this.fromY = 20,
       this.onFinish,
       this.curve = Curves.easeInOut})
       : super(key: key) {
@@ -895,12 +896,12 @@ class ShakeX extends StatefulWidget {
   }
 
   @override
-  ShakeXState createState() => ShakeXState();
+  ShakeXYState createState() => ShakeXYState();
 }
 
 /// State class,
 /// Controls the animations flow
-class ShakeXState extends State<ShakeX>
+class ShakeXYState extends State<ShakeXY>
     with SingleTickerProviderStateMixin, AnimateDoState {
   late Animation<double> shakeAnimation;
 
@@ -916,7 +917,6 @@ class ShakeXState extends State<ShakeX>
     super.initState();
 
     controller = AnimationController(duration: widget.duration, vsync: this);
-
     shakeAnimation = CurvedAnimation(parent: controller, curve: widget.curve);
 
     /// Provided by the mixing [AnimateDoState] class
@@ -946,10 +946,63 @@ class ShakeXState extends State<ShakeX>
         animation: controller,
         builder: (context, child) {
           return Transform.translate(
-            offset: Offset(sin(4 * pi * shakeAnimation.value) * widget.from, 0),
+            offset: Offset(sin(4 * pi * shakeAnimation.value) * widget.fromX, sin(4 * pi * shakeAnimation.value) * widget.fromY),
             child: widget.child,
           );
         });
+  }
+}
+
+
+/// Class [ShakeX]:
+/// [key]: optional widget key reference
+/// [child]: mandatory, widget to animate
+/// [duration]: how much time the animation should take
+/// [delay]: delay before the animation starts
+/// [controller]: optional/mandatory, exposes the animation controller created by Animate_do
+/// the controller can be use to repeat, reverse and anything you want, its just an animation controller
+/// [from] from where you want to start the animation
+/// [infinite] loops the animation until the widget is destroyed
+class ShakeX extends StatelessWidget {
+  final Widget child;
+  final Duration duration;
+  final Duration delay;
+  final bool infinite;
+  final Function(AnimationController)? controller;
+  final bool manualTrigger;
+  final bool animate;
+  final Function(AnimateDoDirection direction)? onFinish;
+  final Curve curve;
+  final double from;
+
+  const ShakeX(
+      {key,
+        required this.child,
+        this.duration = const Duration(milliseconds: 1000),
+        this.delay = const Duration(milliseconds: 0),
+        this.infinite = false,
+        this.controller,
+        this.manualTrigger = false,
+        this.animate = true,
+        this.from = 20,
+        this.onFinish,
+        this.curve = Curves.easeInOut});
+
+  @override
+  Widget build(BuildContext context) {
+    return ShakeXY(
+      child: child,
+      duration: duration,
+      delay: delay,
+      infinite: infinite,
+      controller: controller,
+      manualTrigger: manualTrigger,
+      animate: animate,
+      fromX: from,
+      fromY: 0,
+      onFinish: onFinish,
+      curve: curve,
+    );
   }
 }
 
@@ -962,7 +1015,7 @@ class ShakeXState extends State<ShakeX>
 /// the controller can be use to repeat, reverse and anything you want, its just an animation controller
 /// [from] from where you want to start the animation
 /// [infinite] loops the animation until the widget is destroyed
-class ShakeY extends StatefulWidget {
+class ShakeY extends StatelessWidget {
   final Widget child;
   final Duration duration;
   final Duration delay;
@@ -974,80 +1027,33 @@ class ShakeY extends StatefulWidget {
   final Curve curve;
   final double from;
 
-  ShakeY(
+  const ShakeY(
       {key,
-      required this.child,
-      this.duration = const Duration(milliseconds: 1000),
-      this.delay = const Duration(milliseconds: 0),
-      this.infinite = false,
-      this.controller,
-      this.manualTrigger = false,
-      this.animate = true,
-      this.from = 20,
-      this.onFinish,
-      this.curve = Curves.easeInOut})
-      : super(key: key) {
-    if (manualTrigger == true && controller == null) {
-      throw FlutterError('If you want to use manualTrigger:true, \n\n'
-          'Then you must provide the controller property, that is a callback like:\n\n'
-          ' ( controller: AnimationController) => yourController = controller \n\n');
-    }
-  }
-
-  @override
-  ShakeYState createState() => ShakeYState();
-}
-
-/// State class,
-/// Controls the animations flow
-class ShakeYState extends State<ShakeY>
-    with SingleTickerProviderStateMixin, AnimateDoState {
-  late Animation<double> shakeAnimation;
-
-  @override
-  void dispose() {
-    disposed = true;
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    controller = AnimationController(duration: widget.duration, vsync: this);
-    shakeAnimation = CurvedAnimation(parent: controller, curve: widget.curve);
-
-    /// Provided by the mixing [AnimateDoState] class
-    configAnimation(
-      delay: widget.delay,
-      animate: widget.animate,
-      manualTrigger: widget.manualTrigger,
-      infinite: widget.infinite,
-      onFinish: widget.onFinish,
-      controllerCallback: widget.controller,
-    );
-  }
+        required this.child,
+        this.duration = const Duration(milliseconds: 1000),
+        this.delay = const Duration(milliseconds: 0),
+        this.infinite = false,
+        this.controller,
+        this.manualTrigger = false,
+        this.animate = true,
+        this.from = 20,
+        this.onFinish,
+        this.curve = Curves.easeInOut});
 
   @override
   Widget build(BuildContext context) {
-    /// Provided by the mixing [AnimateDoState] class
-    buildAnimation(
-      delay: widget.delay,
-      animate: widget.animate,
-      manualTrigger: widget.manualTrigger,
-      infinite: widget.infinite,
-      onFinish: widget.onFinish,
-      controllerCallback: widget.controller,
+    return ShakeXY(
+      child: child,
+      duration: duration,
+      delay: delay,
+      infinite: infinite,
+      controller: controller,
+      manualTrigger: manualTrigger,
+      animate: animate,
+      fromX: 0,
+      fromY: from,
+      onFinish: onFinish,
+      curve: curve,
     );
-
-    return AnimatedBuilder(
-        animation: controller,
-        builder: (context, child) {
-          return Transform.translate(
-            offset: Offset(0, sin(4 * pi * shakeAnimation.value) * widget.from),
-            child: widget.child,
-          );
-        });
   }
 }
